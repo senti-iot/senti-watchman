6e2da7bbefbebe3bfb57c66ef7f6d9723707016f
Christian Broberg <cb@webhouse.dk>
2018-10-16 23:31:32 +0200

    Default route added

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { promisify } = require('util')
 
-
 const app = express()
+
 app.use(bodyParser.json())
+
+// Routes will go here
+
+// Default root route
+app.get("/", (req, res, next) => {
+	// res.send('OK')
+	res.end(new Date().toISOString())
+})
 
 const startServer = async () => {
 	const port = process.env.SERVER_PORT || 3000

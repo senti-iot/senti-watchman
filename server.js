636f71742890b75d01d8f9ddf89b86de04a8edb2
Christian Broberg <cb@webhouse.dk>
2018-10-17 14:25:57 +0200

    More cleaning

--- server.js
+++ server.js
@@ -1,27 +1,6 @@
-require('dotenv').config({ path: './.env' })
-const express = require('express')
-const bodyParser = require('body-parser')
-const { promisify } = require('util')
 const watch = require('./lib/watch')
+const startServer = require('./lib/startserver')
 
 watch()
-
-const app = express()
-
-app.use(bodyParser.json())
-
-// Routes will go here
-
-app.get("/", (req, res, next) => {
-	var date = new Date().toISOString()
-	console.log('sending date: ', date)
-	res.end(date)
-})
-
-const startServer = async () => {
-	const port = process.env.SERVER_PORT || 3001
-	await promisify(app.listen).bind(app)(port)
-	console.log(`Senti Watchman Server listening on port ${port}`)
-}
 
 startServer()

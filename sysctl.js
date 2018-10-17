491dcb8c38e116cb7cd8239716e6816a02ddaf44
Christian Broberg <cb@webhouse.dk>
2018-10-17 02:01:52 +0200

    New

--- sysctl.js
+++ sysctl.js
@@ -5,7 +5,4 @@ systemctl.isEnabled('senti-watchman.service').then(enabled => {
 })
 
 // Start a service
-// systemctl.start('senti-watchman.service').then(output => console.log)
-
-output = await systemctl.start('senti-watchman.service')
-console.log(output)
+systemctl.start('senti-watchman.service').then(output => console.log)
